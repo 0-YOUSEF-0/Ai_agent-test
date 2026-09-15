be9973dc1b74153d65fa5cdd4365b38b8c04fe9d
0-YOUSEF-0 <yousefrsamaro5@gmail.com>
2026-09-15 16:41:08 +0300

    get file content

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,19 +1,39 @@
-import os 
+import os
 
 def get_files_info(working_directory: str, directory: str = ".") -> str:
-  try :
-   working_dir_abs = os.path.abspath(working_directory)
-   target_dir = os.path.normpath(os.path.join(working_dir_abs, directory))
-   valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
+    try:
+        working_dir_abs = os.path.abspath(working_directory)
 
-   #  Will be True or False
+        target_dir = os.path.normpath(
+            os.path.join(working_dir_abs, directory)
+        )
 
-   if not  valid_target_dir : 
-     return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
+        valid_target_dir = (
+            os.path.commonpath([working_dir_abs, target_dir])
+            == working_dir_abs
+        )
 
-   if not os.path.isdir(target_dir):
-     return f'Error: "{directory}" is not a directory'
+        if not valid_target_dir:
+            return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-   return f'Success: "{directory}" is within the working directory'
-  except Exception as e:
+        if not os.path.isdir(target_dir):
+            return f'Error: "{directory}" is not a directory'
+
+        items = []
+
+        for item in os.listdir(target_dir):
+            item_path = os.path.join(target_dir, item)
+
+            name = item
+            size = os.path.getsize(item_path)
+            is_directory = os.path.isdir(item_path)
+
+            item_info = f"- {name}: file_size={size} bytes, is_dir={is_directory}"
+            items.append(item_info)
+
+        return "\n".join(items)
+
+    except Exception as e:
         return f"Error: {e}"
+
+
